104248818d1cf22a7da8a241adb8475b8190ae88
Judith Lloyd-Jones <j.r.lloydjones@gmail.com>
2018-12-05 18:16:09 +0000

    Enable sessions and get /set view

--- app.rb
+++ app.rb
@@ -1,10 +1,23 @@
+# App Controller
 require 'sinatra'
+require './lib/memory.rb'
 
 class App < Sinatra::Base
   set :port, 4000
+  enable :sessions
+
+  before do
+    session[:memory] ||= Memory.new
+  end
 
   get '/' do
     "Hello world"
+  end
+
+  get '/set' do
+    @memory = session[:memory]
+    @memory.save_data(params)
+    erb :set
   end
 
   run! if app_file == $0
