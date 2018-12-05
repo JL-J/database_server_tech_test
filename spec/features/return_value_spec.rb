af55dabe8ce354d1edd77f82cb101aa1c76147fd
Judith Lloyd-Jones <j.r.lloydjones@gmail.com>
2018-12-05 18:50:17 +0000

    Test /get view to return key-value pair

---
+++ spec/features/return_value_spec.rb
@@ -0,0 +1,7 @@
+feature '/get?' do
+  scenario 'user can retrieve a value from the memory when providing a key' do
+    visit '/set?colour=red'
+    visit '/get?somekey=colour'
+    expect(page).to have_content 'red'
+  end
+end
