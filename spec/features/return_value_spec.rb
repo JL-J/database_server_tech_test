eb9f23612b073542cb739d36faddb4e3d5d71959
Judith Lloyd-Jones <j.r.lloydjones@gmail.com>
2018-12-06 08:16:11 +0000

    Update somekey to key in test

--- spec/features/return_value_spec.rb
+++ spec/features/return_value_spec.rb
@@ -1,7 +1,7 @@
 feature '/get?' do
   scenario 'user can retrieve a value from the memory when providing a key' do
     visit '/set?colour=red'
-    visit '/get?somekey=colour'
+    visit '/get?key=colour'
     expect(page).to have_content 'red'
   end
 end
